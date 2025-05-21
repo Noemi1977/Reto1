ed5fd178c4b56bbb39c18b17dd592d29d85686c0
Noemi1977 <noemi.casaprima.pendas@gmail.com>
2025-05-21 19:50:27 +0100

    commented code changes

--- src/fundamentos_python_I.py
+++ src/fundamentos_python_I.py
@@ -1,115 +1,115 @@
 # # Ejercicio 1, identificar tipos básicos
-# a="Hola";
-# b=25;
-# c=3.14;
-# d=True;
-# e=None;
-# print(type(a));
-# print(type(b));
-# print(type(c));
-# print(type(d));
-# print(type(e));
+a="Hola";
+b=25;
+c=3.14;
+d=True;
+e=None;
+print(type(a));
+print(type(b));
+print(type(c));
+print(type(d));
+print(type(e));
 
 # # Ejercicio 2, practicar conversiones entre tipos.
 
-# cadena="42";
-# cad_integer= int(cadena);
-# print(cad_integer);
+cadena="42";
+cad_integer= int(cadena);
+print(cad_integer);
 
-# suma_cad=cad_integer+8;
-# print(suma_cad);
+suma_cad=cad_integer+8;
+print(suma_cad);
 
-# num=100;
-# num_cad= str(num);
-# print("Tu puntuacion es ", num_cad);
+num=100;
+num_cad= str(num);
+print("Tu puntuacion es ", num_cad);
 
 # # Ejercicio 3, nombres y saludos
 
-# nombre="Noemi";
-# edad=47;
-# print ("Hola me llamo ", nombre, "y tengo ", edad, " años");
+nombre="Noemi";
+edad=47;
+print ("Hola me llamo ", nombre, "y tengo ", edad, " años");
 
 # # Ejercicio 4, Reasignar valores de forma clara
-# x="gato";
-# y="perro";
-# print("Imprimeme el valor de x: ", x);
-# print("Imprimeme el valor de y: ", y);
+x="gato";
+y="perro";
+print("Imprimeme el valor de x: ", x);
+print("Imprimeme el valor de y: ", y);
 
-# x="perro";
-# y="gato";
+x="perro";
+y="gato";
 
-# print("Imprimeme el valor de x: ", x);
-# print("Imprimeme el valor de y: ", y);
+print("Imprimeme el valor de x: ", x);
+print("Imprimeme el valor de y: ", y);
 
 # # Ejercicio 5. Suma de la compra
 
-# pan= 1.20;
-# leche=0.95;
-# huevos=2.10;
+pan= 1.20;
+leche=0.95;
+huevos=2.10;
 
-# total= pan+leche+huevos;
-# print("El total de tu compra es: ", total);
+total= pan+leche+huevos;
+print("El total de tu compra es: ", total);
 
 # # Ejercicio 6. ¿Par o impar?
 
-# numero= input("Introduce un numero: ");
-# if int(numero) % 2 == 0:
-#     print("El numero es par");
-# else:
-#     print("El numero es impar");
+numero= input("Introduce un numero: ");
+if int(numero) % 2 == 0:
+    print("El numero es par");
+else:
+    print("El numero es impar");
 
 # Ejercicio 7. ¿Mayor de edad?
-# edad= input("Introduce tu edad: ");
-# if int(edad) >= 18:
-#     print("Eres mayor de edad");
-# else:
-#     print("Eres menor de edad");
+edad= input("Introduce tu edad: ");
+if int(edad) >= 18:
+    print("Eres mayor de edad");
+else:
+    print("Eres menor de edad");
 
 # Ejercicio 8. ¿Elige una opción?
 
-# seleccion= input("Ingresa una opción del 1 al 3:");
-# if seleccion == "1":
-#     print("Ver perfil");
-# elif seleccion == "2":
-#     print("Editar perfil");
-# elif seleccion == "3":
-#     print("Cerrar sesión")
-# else :
-#     print("No has marcado ninguna opcion valida");
+seleccion= input("Ingresa una opción del 1 al 3:");
+if seleccion == "1":
+    print("Ver perfil");
+elif seleccion == "2":
+    print("Editar perfil");
+elif seleccion == "3":
+    print("Cerrar sesión")
+else :
+    print("No has marcado ninguna opcion valida");
 
 # Ejercicio 9. Detector de tipos raros
 
-# def verificar_tipo(valor):
-#     if(isinstance(valor,str)):
-#         print("El valor introducido es una cadena de texto");
-#     elif isinstance(valor,int):
-#         print("El valor introducido es un número entero");
-#     elif isinstance(valor,float):
-#         print("El valor introducido es un numero decimal");
-#     elif isinstance(valor,bool):
-#         print("El valor introducido es un booleano");
-#     elif valor is None:
-#         print("El valor introducido es nulo");
-#     else:
-#         print("No reconozco el valor que has introducido");
+def verificar_tipo(valor):
+    if(isinstance(valor,str)):
+        print("El valor introducido es una cadena de texto");
+    elif isinstance(valor,int):
+        print("El valor introducido es un número entero");
+    elif isinstance(valor,float):
+        print("El valor introducido es un numero decimal");
+    elif isinstance(valor,bool):
+        print("El valor introducido es un booleano");
+    elif valor is None:
+        print("El valor introducido es nulo");
+    else:
+        print("No reconozco el valor que has introducido");
 
-# verificar_tipo("Hola")
-# verificar_tipo(25)
-# verificar_tipo(3.14)
-# verificar_tipo(True)
-# verificar_tipo(None)
+verificar_tipo("Hola")
+verificar_tipo(25)
+verificar_tipo(3.14)
+verificar_tipo(True)
+verificar_tipo(None)
 
-# Otra forma de hacerlo
-# dato= input("Introduce un dato:")
+#Otra forma de hacerlo
+dato= input("Introduce un dato:")
 
-# try:
-#     if dato.isdigit():
-#         dato=int(dato)
-#     elif dato.replace(".","",1).isdigit():
-#         dato=float(dato)
-#     verificar_tipo(dato)
-# except ValueError:
-#     print("El valor introducido no es un número válido")
+try:
+    if dato.isdigit():
+        dato=int(dato)
+    elif dato.replace(".","",1).isdigit():
+        dato=float(dato)
+    verificar_tipo(dato)
+except ValueError:
+    print("El valor introducido no es un número válido")
 
 # ✨ Ejercicio 10: Calculadora con menú
 # Pide dos números y muestra este menú:
@@ -120,34 +120,34 @@
 # Según la opción elegida, haz la operación y muestra el resultado.
 # Bonus: si elige dividir y el segundo número es 0, muestra “No se puede dividir por cero”.
 
-# def calculadora():
-#     number1 = float(input("Introduce el primer número: "))
-#     number2 = float(input("Introduce el segundo número: "))
-#     print("Elige una opción:")
-#     print("1. Sumar")
-#     print("2. Restar")
-#     print("3. Multiplicar")
-#     print("4. Dividir")
-#     opcion = input("Opción elegida: ")
-#     if opcion == "1":
-#         resultado = number1 + number2
-#         print(f"La suma es: {resultado}")
-#     elif opcion == "2":
-#         resultado = number1 - number2
-#         print(f"La resta es: {resultado}")
-#     elif opcion == "3":
-#         resultado = number1 * number2
-#         print(f"La multiplicación es: {resultado}")
-#     elif opcion == "4":
-#         if number2 != 0:
-#             resultado = number1 / number2
-#             print(f"La división es: {resultado}")
-#         else:
-#             print("No se puede dividir por cero.")
-#     else:
-#         print("Opción no válida.")
+def calculadora():
+    number1 = float(input("Introduce el primer número: "))
+    number2 = float(input("Introduce el segundo número: "))
+    print("Elige una opción:")
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    opcion = input("Opción elegida: ")
+    if opcion == "1":
+        resultado = number1 + number2
+        print(f"La suma es: {resultado}")
+    elif opcion == "2":
+        resultado = number1 - number2
+        print(f"La resta es: {resultado}")
+    elif opcion == "3":
+        resultado = number1 * number2
+        print(f"La multiplicación es: {resultado}")
+    elif opcion == "4":
+        if number2 != 0:
+            resultado = number1 / number2
+            print(f"La división es: {resultado}")
+        else:
+            print("No se puede dividir por cero.")
+    else:
+        print("Opción no válida.")
 
-# calculadora()        
+calculadora()        
 
 # ✨ Ejercicio 11: Clasificador de edad
 # Pide al usuario su edad y clasifícalo:
